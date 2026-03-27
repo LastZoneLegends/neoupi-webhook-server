7f7a8f609cf8283318a51f5f2807cbaeea265f40
LastZoneLegends <lastzonelegends0@gmail.com>
2026-03-27 09:15:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 }
 
+let db = null;
+
 if (serviceAccount) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
 
-const db = admin.firestore();
+  db = admin.firestore();
+}
 
 /*
 Health check route
